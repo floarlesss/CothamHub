b45b0242cc964dca98972b6af6e4dfb63a3c5798
floarlesss <117862164+floarlesss@users.noreply.github.com>
2023-09-22 00:04:23 +0100

    Still in development. Won't do detailed descriptions this early.

---
+++ CothamHub/CothamHub.py
@@ -0,0 +1,11 @@
+import os
+import urllib.request as urllib2
+from pathlib import Path
+from configparser import ConfigParser
+from datetime import datetime
+
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import *
+
+
